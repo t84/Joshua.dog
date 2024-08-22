0143a5180d8b4bf713ca23a436d6f593aa6d96f6
Slakoth <69316271+t84@users.noreply.github.com>
2024-08-22 17:00:53 -0500

    added site wide dark mode

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
     const darkModeButton = document.querySelector('#darkModeButton');
 
-    // Load dark mode preference from localStorage
     const darkMode = localStorage.getItem('darkMode');
     if (darkMode === 'enabled') {
         document.body.classList.add('dark-mode');
-        darkModeButton.textContent = '☀️'; // Update button text
+        darkModeButton.textContent = '☀️';
     }
 
     darkModeButton.addEventListener('click', function() {
         document.body.classList.toggle('dark-mode');
 
-        // Update button text
         if (document.body.classList.contains('dark-mode')) {
             localStorage.setItem('darkMode', 'enabled');
             darkModeButton.textContent = '☀️';
